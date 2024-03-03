228551a6747b0f25a2a1ba2773ac2d699b12abe2
ABHIJEET-KUMAR-XdYZg <133910196+ABHIJEET-KUMAR-XdYZg@users.noreply.github.com>
2024-03-03 10:56:40 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAimxnm-IjTs8sTVe5yy_8udwIiIvnsQ8o",
+
+  authDomain: "nitr-2f018.firebaseapp.com",
+
+  databaseURL: "https://nitr-2f018-default-rtdb.firebaseio.com",
+
+  projectId: "nitr-2f018",
+
+  storageBucket: "nitr-2f018.appspot.com",
+
+  messagingSenderId: "1008777056920",
+
+  appId: "1:1008777056920:web:75e5253af87f50963136ed",
+
+  measurementId: "G-5RY8HFS5V9"
+
+};
+export {firebaseConfig};
